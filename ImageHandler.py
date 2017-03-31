dfc82a7f76118a9f648cb99fbe1cc782b4bdddf2
elfyyy <kyleh04@ymail.com>
2017-03-31 09:07:45 -0400

    Cleans up code and optimizes some stuff
    More optimization may be needed deping on speed of pi

--- ImageHandler.py
+++ ImageHandler.py
@@ -1,7 +1,7 @@
 '''
 *   ImageHandler.py handles basic image taking and processing
 *
-*   No image processing is done within
+*   Handles all color conversions, outputs to out_image
 *
 *   Currently a place holder loading a static image
 '''
@@ -13,18 +13,22 @@ class ImageHandler:
     def __init__(self, testing):
         if not testing:
             print("Error: Not Implemented")
-        self.testing, self.image = testing, None
-
-    def resize_image(self, width, height):
-        self.image = cv2.resize(self.image, (width, height))
+        self.testing, self.out_image, self._original_image = testing, None, None
 
     def take_image(self):
         if self.testing:
-            self.image = cv2.imread('simulated_launch_1500ft.jpg', cv2.IMREAD_COLOR)
+            self._original_image = cv2.imread('simulated_launch_1500ft.jpg', cv2.IMREAD_COLOR)
+            self.out_image = self._original_image
         else:
             pass
 
     def show_image(self):
-        cv2.imshow('image', self.image)
+        cv2.imshow('image', self.out_image)
         cv2.waitKey(0)
         cv2.destroyAllWindows()
+
+    def cvt_to_grayscale(self):
+        return cv2.cvtColor(self._original_image, cv2.COLOR_BGR2GRAY)
+
+    def cvt_to_hsv(self):
+        return cv2.cvtColor(self._original_image, cv2.COLOR_BGR2HSV)
--- ImageProcessor.py
+++ ImageProcessor.py
@@ -14,36 +14,20 @@ class ImageProcessor:
     def __init__(self, ih):
         self.ImageHandler = ih
         self.processed_image, self.grayscale_image, self.hsv_image, self.contours, self.mask, self.threshold_image, \
-            self.red_mask, self.blue_mask, self.yellow_mask = None, None, None, [], None, None, None, None, None
-
-    def cvt_grayscale(self):
-        self.grayscale_image = cv2.cvtColor(self.ImageHandler.image, cv2.COLOR_BGR2GRAY)
-        self.processed_image = self.grayscale_image
-
-    def cvt_hsv(self):
-        self.hsv_image = cv2.cvtColor(self.ImageHandler.image, cv2.COLOR_BGR2HSV)
-        self.processed_image = self.hsv_image
+            self.tarp_masks = None, None, None, [], None, None, [0, 0, 0]
 
     def filter_hsv(self, lower, upper):
         self.processed_image = cv2.inRange(self.hsv_image, lower, upper)
 
     def get_hsv_avg(self):
-        x, y, _ = self.hsv_image.shape
         h_avg, s_avg, v_avg = np.average(np.average(self.hsv_image, 0), 0)
         h_avg, s_avg, v_avg = int(h_avg), int(s_avg), int(v_avg)
         return h_avg, s_avg, v_avg
 
-    def get_background_h_high(self):
-        return int(self.get_hsv_avg()[0] + 5)
-
-    def hsv_thresh(self, low, high):
-        pass
-
     def create_tarp_mask(self):
-        self.cvt_grayscale()
-        self.cvt_hsv()
-        h_high = self.get_background_h_high()
-        self.mask = cv2.inRange(self.hsv_image, (h_high, 0, 0), (180, 255, 255))
+        self.hsv_image = self.ImageHandler.cvt_to_hsv()
+        h_high = self.get_hsv_avg()[0] + 5
+        self.mask = cv2.inRange(self.hsv_image, (h_high, 0, 0), (180, 255, 255))  # Creates mask removing background
         self.processed_image = cv2.bitwise_and(self.hsv_image, self.hsv_image, mask=self.mask)
         self.grayscale_image = cv2.cvtColor(self.processed_image, cv2.COLOR_BGR2GRAY)
         _, self.threshold_image = cv2.threshold(self.grayscale_image, 1, 255, cv2.THRESH_BINARY)
@@ -57,62 +41,39 @@ class ImageProcessor:
             i += 1
         self.grayscale_image = cv2.cvtColor(self.processed_image, cv2.COLOR_BGR2GRAY)
         _, self.mask = cv2.threshold(self.grayscale_image, 254, 255, cv2.THRESH_BINARY)
+        # Creates mask without background & foreground outside of size
 
     def get_tarps(self):
-        temp = cv2.bitwise_and(self.hsv_image, self.hsv_image, mask=self.mask)
-        y, x, _ = temp.shape
+        temp = cv2.bitwise_and(self.hsv_image, self.hsv_image, mask=self.mask)  # Masks off background on HSV image
         self.processed_image = temp
+        y, x, _ = temp.shape
         temp = cv2.resize(temp, (int(x * 0.5), int(y * 0.5)))
         x, y, _ = temp.shape
-        mylist = []
-        for i in range(0, x):
-            for j in range(0, y):
-                if temp[i, j, 0] != 0:
-                    mylist.append(temp[i, j, 0])
 
-        counter = collections.Counter(mylist)
-
+        hue_vals = [temp[i, j, 0] for i in range(0, x) for j in range(0, y) if temp[i, j, 0] != 0]
+        counter = collections.Counter(hue_vals)
         tarp_hues = self.get_section_means(counter.most_common(50))
+
         if len(tarp_hues) == 3:
-            print('Thank God')
-
-            self.yellow_mask = cv2.inRange(self.processed_image,
-                                           (int(tarp_hues[0] - 10), 0, 0), (int(tarp_hues[0] + 10), 255, 255))
-
-            self.blue_mask = cv2.inRange(self.processed_image,
-                                         (int(tarp_hues[1] - 10), 0, 0), (int(tarp_hues[1] + 10), 255, 255))
-
-            self.red_mask = cv2.inRange(self.processed_image,
-                                        (int(tarp_hues[2] - 10), 0, 0), (int(tarp_hues[2] + 10), 255, 255))
+            for i in range(0, 3):  # y, b, r
+                self.tarp_masks[i] = cv2.inRange(self.processed_image,
+                                                 (int(tarp_hues[i] - 10), 0, 0), (int(tarp_hues[i] + 10), 255, 255))
+        else:
+            print("Too many bins!")
 
     def save_tarps(self):
-        yellow = self.ImageHandler.image.copy()
-        _, cnts, _ = cv2.findContours(self.yellow_mask.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-        cv2.drawContours(yellow, cnts, -1, (0, 255, 0), 2)
+        files = ['yellow', 'blue', 'red']
 
-        cv2.imwrite("out/yellow.png", yellow)
+        for i in range(0, 3):
+            img = self.ImageHandler.out_image.copy()
+            _, cnts, _ = cv2.findContours(self.tarp_masks[i].copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+            cv2.drawContours(img, cnts, -1, (0, 255, 0), 2)
 
-        blue = self.ImageHandler.image.copy()
-        _, cnts, _ = cv2.findContours(self.blue_mask.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-        cv2.drawContours(blue, cnts, -1, (0, 255, 0), 2)
+            cv2.imwrite("out/" + str(files[i]) + ".png", img)
 
-        cv2.imwrite("out/blue.png", blue)
-
-        red = self.ImageHandler.image.copy()
-        _, cnts, _ = cv2.findContours(self.red_mask.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-        cv2.drawContours(red, cnts, -1, (0, 255, 0), 2)
-
-        cv2.imwrite("out/red.png", red)
-
-    def get_section_means(self, coll):
-        nums = self.get_section_numbers(coll)
-        nums[nums == 0] = np.nan
-        val = np.nanmean(nums, axis=1)
-        val = val[~np.isnan(val)]
-        return val
-
-    def get_section_numbers(self, coll):
-        nums = [coll[x][0] for x in range(0, len(coll)) if coll[x][1] > 30]
+    @staticmethod
+    def get_section_numbers(coll):
+        nums = [coll[x][0] for x in range(0, len(coll)) if coll[x][1] > 30]  #  Only gets Hues with frequency > 30
         nums = np.sort(nums)
         means = np.zeros((10, 10))
         i, j = 0, 0
@@ -127,9 +88,13 @@ class ImageProcessor:
                 means[i, j] = nums[s]
         return means
 
+    def get_section_means(self, coll):
+        nums = self.get_section_numbers(coll)
+        nums[nums == 0] = np.nan
+        val = np.nanmean(nums, axis=1)
+        val = val[~np.isnan(val)]
+        return val
+
     def debug(self):
-        # cv2.imshow("Original", self.ImageHandler.image)
         cv2.imshow("Processed", self.processed_image)
-        # cv2.imshow("ssa", self.edges)
-        # cv2.imshow("Mask", self.mask)
         cv2.waitKey(0)
