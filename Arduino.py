63691afc6729e28bd884e2766178fc9846540d82
elfyyy <kyleh04@ymail.com>
2017-04-03 06:05:17 -0400

    Adds Arduino.py
    Uses i2c protocol to communicate with Arduino

---
+++ Arduino.py
@@ -0,0 +1,31 @@
+import smbus
+import time
+
+
+class Arduino:
+    bus = smbus.SMBus(1)
+    address = 0x04
+    ALTREQONE, ALTREQTWO, ERRORCODEREQ, DISTREQONE, DISTREQTWO = 0, 1, 2, 3, 4
+
+    def write_number(self, value):
+        self.bus.write_byte(self.address, value)
+
+    def read_number(self):
+        number = self.bus.read_byte(self.address)
+        return number
+
+    def get_altitude(self):
+        self.write_number(self.ALTREQONE)
+        byte_one = self.read_number()
+
+        self.write_number(self.ALTREQTWO)
+        byte_two = self.read_number()
+        return int(str(byte_one) + str(byte_two))
+
+    def get_distance(self):
+        self.write_number(self.DISTREQONE)
+        byte_one = self.read_number()
+
+        self.write_number(self.DISTREQTWO)
+        byte_two = self.read_number()
+        return int(str(byte_one) + str(byte_two))
