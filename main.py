050b7b137fbee26423ba3a2dd81138e0285520df
Kyle <kylehunter@mail.usf.edu>
2017-04-07 23:55:22 -0400

    Adds secondary logger
    Use soley for logging our orientations throughout the flight after ejection

--- main.py
+++ main.py
@@ -10,10 +10,13 @@ import TarpCalculation as Tc
 from Arduino import Arduino
 from ErrorIndicator import ErrorIndicator
 import time
-import logging
 import os
 import shutil
 import RPi.GPIO as GPIO
+import Log
+import logging
+
+main_logger, orientation_logger = logging.getLogger("SOAR.main_logger"), logging.getLogger("SOAR.orientation_logger")
 
 
 class main:
@@ -36,8 +39,7 @@ class main:
         if os.path.exists("log.txt"):
             os.remove("log.txt")
 
-        logging.basicConfig(filename='log.txt', level=logging.DEBUG,
-                            format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
+        Log.setup_log()
 
     def check_for_gps_fix(self):
         """
@@ -45,7 +47,7 @@ class main:
         :return: True if GPS has a fix
         """
         if not main.arduino.gps_has_fix():
-            logging.info("Waiting for GPS fix..")
+            main_logger.info("Waiting for GPS fix..")
             self.ei.message([0, 0, 0, 1])
             time.sleep(0.5)
             self.ei.message([0, 0, 0, 0])
@@ -90,22 +92,22 @@ class main:
         Method run while lander is in decent
         :param count: Number of loops called (Used for logger)
         """
-        logging.info("*********************" + str(count) + "*********************")
+        main_logger.info("*********************" + str(count) + "*********************")
         current_altitude = self.arduino.get_altitude() * 3.28084  # ft
-        logging.info("Current Altitude: " + str(current_altitude))
+        main_logger.info("Current Altitude: " + str(current_altitude))
 
         current_distance = self.arduino.get_distance() * 3.28084  # ft
-        logging.info("Current Distance: " + str(current_distance))
+        main_logger.info("Current Distance: " + str(current_distance))
 
         total_tarp_area = Tc.get_total_tarp_area(current_altitude, current_distance)  # pixels^3
 
-        logging.info("Creating Background Mask")
+        main_logger.info("Creating Background Mask")
         self.processor.create_background_mask()
-        logging.info("Filtering by size")
+        main_logger.info("Filtering by size")
         self.processor.filter_by_size(total_tarp_area)
-        logging.info("Getting Tarp Mask")
+        main_logger.info("Getting Tarp Mask")
         self.processor.get_tarps()
-        logging.info("Saving Tarps")
+        main_logger.info("Saving Tarps")
         self.processor.save_tarps(count)
 
     @staticmethod
@@ -187,7 +189,7 @@ main = main()
 #  Waits for switch to tell Pi to have everything start
 while not main.told_to_start():
     time.sleep(3)
-logging.info("Initializing main")
+main_logger.info("Initializing main")
 main.starting_notification()
 
 
@@ -195,24 +197,24 @@ main.starting_notification()
 if not main.init():
     while not main.check_arduino(False) or not main.check_pi():
         time.sleep(3)
-        logging.info("Arduino or Pi have errors..")
+        main_logger.info("Arduino or Pi have errors..")
         if main.told_to_end():  # Just in case..
             break
-logging.info("Main Initialized")
+main_logger.info("Main Initialized")
 
 
 #  Waits outside rocket, checking for errors and GPS fix
 in_count = 0
 while in_count < 20:  # 20*3 = 60s of consistent darkness
-    logging.info("Waiting to be inside rocket")
+    main_logger.info("Waiting to be inside rocket")
     while not main.arduino.gps_has_fix():
         main.check_for_gps_fix()
-        logging.info("Outside rocket, no GPS fix..")
+        main_logger.info("Outside rocket, no GPS fix..")
 
     if main.check_arduino(False) and main.check_pi():
         main.ei.message([0, 0, 0, 1])
     else:
-        logging.info("Arduino or Pi have errors..")
+        main_logger.info("Arduino or Pi have errors..")
     time.sleep(3)
     if main.told_to_end():  # Just in case..
         break
@@ -220,32 +222,35 @@ while in_count < 20:  # 20*3 = 60s of consistent darkness
         in_count += 1
     else:
         in_count = 0
-logging.info("Inside Rocket")
+main_logger.info("Inside Rocket")
 main.second_notification()
 
 
 #  Lander is inside rocket, still checks for errors with arduino(Can hear buzzer)
 while main.is_in_rocket():
-    logging.info("Waiting for Ejection")
+    main_logger.info("Waiting for Ejection")
     if not main.arduino.gps_has_fix():
-        logging.info("Inside rocket, no GPS fix..")
+        main_logger.info("Inside rocket, no GPS fix..")
     if main.check_arduino(False):
         main.ei.message([0, 0, 0, 0])  # Can't see the error anyway..
-        logging.info("Inside Rocket, error with Arduino")
+        main_logger.info("Inside Rocket, error with Arduino")
     time.sleep(3)
     if main.told_to_end():  # Just in case..
         break
-logging.info("Ejected!")
+main_logger.info("Ejected!")
 main.third_notification()
 
 #  Lander is outside rocket, no error checks as it's irrelevant, takes images
 time.sleep(15)  # Ensure parachute and everything is cleared before initializing camera servos TODO Determine how long
 counter = 0
+start_time = time.time()
 while True:
+    run_time = time.time() - start_time
     loop_time = time.time()
-    logging.info("GPS: " + str(main.arduino.get_lattitude) + ", " + str(main.arduino.get_longitude))
+    main_logger.info("GPS: " + str(main.arduino.get_lattitude) + ", " + str(main.arduino.get_longitude))
+    orientation_logger.info(Log.o_format(run_time, main.arduino.get_orientations()))
     if main.is_at_low_altitude():
-        logging.info("Below 10m, Quiting!")
+        main_logger.info("Below 10m, Quiting!")
         break
     main.run(counter)
     counter += 1
@@ -257,5 +262,5 @@ while True:
 
 main.arduino.shutdown()
 main.processor.scores = sorted(main.processor.scores)
-logging.info("Scores (First is best): " + str(main.processor.scores))
+main_logger.info("Scores (First is best): " + str(main.processor.scores))
 
